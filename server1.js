121ba9c430e75f3eb1e0ac6be9d493b9ac5281e2
yangzhanmei <1724617718@qq.com>
2017-04-23 16:45:17 +0800

    delete annotate

--- server1.js
+++ server1.js
@@ -2,10 +2,6 @@ var express = require("express");
 var app =  express();
 
 app.use(express.static('./'));
-//
-// app.get("/",function (req,res) {
-//     res.sendFile("./a.html");
-// });
 
 var server = app.listen(3000,function () {
     var port = server.address().port;
